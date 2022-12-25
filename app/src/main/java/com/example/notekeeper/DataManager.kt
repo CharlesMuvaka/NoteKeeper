4591f9c6f04a412641014c56bd402a0f0b32f3a8
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-26 02:16:39 +0300

    create a method to initialise a course and put it in the map.

--- app/src/main/java/com/example/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -4,5 +4,14 @@ class DataManager {
     private val courses = HashMap<String, Course>()
     val notes = ArrayList<NoteInfo>()
 
-
+    private fun initializeCourses(){
+        val course = Course("android_intents", "Passing data using intents")
+        courses[course.courseId] = course
+        val course1 = Course("java_core", "Java programming fundamentals: The core Platform")
+        courses[course1.courseId] = course1
+        val course2 = Course("android_async", "Android async programming with services")
+        courses[course2.courseId] = course2
+        val course3 = Course("java_lang","Java programming Fundamentals: The java Language")
+        courses[course3.courseId] = course3
+    }
 }
