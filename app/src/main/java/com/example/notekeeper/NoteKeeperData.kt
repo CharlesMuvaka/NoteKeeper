13fd14978e1fc978b4ffbddb79cec1d4e1d4a7c6
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-26 12:30:13 +0300

    deleted mistakenly

--- app/src/main/java/com/example/notekeeper/NoteKeeperData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.notekeeper
-// A file helps in storing many simple classes together
-
-class Course(val courseId: String, val courseTitle: String)
-
-class NoteInfo(var course:Course, var title: String, var text: String)
