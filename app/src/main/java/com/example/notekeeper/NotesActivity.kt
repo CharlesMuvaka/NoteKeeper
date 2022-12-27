a5ab89bc809d623440d6dfab8b65471d4f28414a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-27 21:17:43 +0300

    bind the notes data to the recycler view using the recycler view adapter.

--- app/src/main/java/com/example/notekeeper/NotesActivity.kt
+++ app/src/main/java/com/example/notekeeper/NotesActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notekeeper.adapters.NotesRecAdapter
 import com.example.notekeeper.databinding.ActivityNotesBinding
 
 class NotesActivity : AppCompatActivity(), View.OnClickListener {
@@ -16,6 +17,7 @@ class NotesActivity : AppCompatActivity(), View.OnClickListener {
 
         bind.fab.setOnClickListener(this)
         bind.recView.layoutManager = LinearLayoutManager(this)
+        bind.recView.adapter = NotesRecAdapter(this, DataManager.notes)
     }
 
     override fun onClick(p0: View?) {
