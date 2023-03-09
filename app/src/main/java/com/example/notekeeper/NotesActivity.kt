65de3a7591b7fd47d4c1cba0ff435bfbea557d2b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-09 04:30:13 +0300

    ft: set up the nav controller with the drawer and the action bar.

--- app/src/main/java/com/example/notekeeper/NotesActivity.kt
+++ app/src/main/java/com/example/notekeeper/NotesActivity.kt
@@ -4,20 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notekeeper.adapters.NotesRecAdapter
 import com.example.notekeeper.databinding.ActivityNotesBinding
+import com.google.android.material.navigation.NavigationView
 
 class NotesActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var bind: ActivityNotesBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityNotesBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
-        bind.fab.setOnClickListener(this)
-        bind.recView.layoutManager = LinearLayoutManager(this)
-        bind.recView.adapter = NotesRecAdapter(this, DataManager.notes)
+        val navHost:NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController:NavController = findNavController(R.id.fragment)
+        appBarConfiguration = AppBarConfiguration(navHost.navController.graph, bind.drawerLayout)
+        val drawer = findViewById<NavigationView>(R.id.drawer)
+        drawer.setupWithNavController(navHost.navController)
+
+        setupActionBarWithNavController(navHost.navController, appBarConfiguration)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController:NavController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onClick(p0: View?) {
