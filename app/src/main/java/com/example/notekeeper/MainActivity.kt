1d62c04728b3aba82dc315b92607a0c5f34562c1
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-09 04:28:42 +0300

    ft: extend the transformationAppCompat class.

--- app/src/main/java/com/example/notekeeper/MainActivity.kt
+++ app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.example.notekeeper.databinding.ActivityMainBinding
+import com.skydoves.transformationlayout.TransformationAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : TransformationAppCompatActivity() {
     private lateinit var bind:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
