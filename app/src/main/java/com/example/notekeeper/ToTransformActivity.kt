94247644c12edceaa211414d569d61ca190d094f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-10 23:45:33 +0300

    ft: implement the transformation app compatActivity interface.

---
+++ app/src/main/java/com/example/notekeeper/ToTransformActivity.kt
@@ -0,0 +1,35 @@
+package com.example.notekeeper
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.example.notekeeper.databinding.ActivityToTransformBinding
+import com.skydoves.transformationlayout.TransformationAppCompatActivity
+
+class ToTransformActivity : TransformationAppCompatActivity() {
+    private lateinit var bind: ActivityToTransformBinding
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind = ActivityToTransformBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        val books = DataManager.books
+        val intent = intent
+        val bookTitle = intent.getStringExtra("book")
+
+        for (book in books){
+            if (bookTitle == book.title){
+                bind.image.setImageResource(book.imageURL!!)
+                bind.title.text = book.title
+                bind.description.text = book.description
+                break
+            }
+        }
+
+
+
+    }
+}
