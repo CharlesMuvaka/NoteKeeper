d5966ab961410f456e36b708bb5d6de42dbddf64
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-10 11:14:02 +0300

    ft: add an onclick listener to the recycler view.

--- app/src/main/java/com/example/notekeeper/adapters/BooksRecAdapter.kt
+++ app/src/main/java/com/example/notekeeper/adapters/BooksRecAdapter.kt
@@ -1,19 +1,36 @@
 package com.example.notekeeper.adapters
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notekeeper.Book
+import com.example.notekeeper.R
 import com.example.notekeeper.databinding.BookRecBinding
+import com.example.notekeeper.fragments.BookFragment
 
 class BooksRecAdapter: RecyclerView.Adapter<BooksRecAdapter.MyHolder>() {
 
-    inner class MyHolder(private val bind: BookRecBinding): RecyclerView.ViewHolder(bind.root) {
+    inner class MyHolder(private val bind: BookRecBinding): RecyclerView.ViewHolder(bind.root), View.OnClickListener {
+        val bundle =  Bundle()
         fun setData(book: Book){
+            bind.image.setImageResource(book.imageURL!!)
             bind.title.text = book.title
             bind.author.text = book.author
+            bundle.putSerializable("book", book)
+
+            bind.root.setOnClickListener(this::onClick)
+        }
+
+        override fun onClick(p0: View?) {
+            if (p0 == bind.root){
+                p0.findFragment<BookFragment>().findNavController().navigate(R.id.action_bookFragment_to_singleBokFragment, bundle)
+            }
         }
     }
 
