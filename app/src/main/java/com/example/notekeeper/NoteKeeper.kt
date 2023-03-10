428dfc209de8e5201aa690beacfb190a1a4c0ff8
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-10 11:12:16 +0300

    ft: implement the serializable interface on the book blue print.

--- app/src/main/java/com/example/notekeeper/NoteKeeper.kt
+++ app/src/main/java/com/example/notekeeper/NoteKeeper.kt
@@ -8,7 +8,7 @@ data class CourseInfo(val courseId:String, val courseTitle:String){
 
 data class NoteInfo(var course:CourseInfo, var noteTitle:String, var noteDescription:String)
 
-data class Book(val title: String, val author: String){
+data class Book(val title: String, val author: String): java.io.Serializable{
     var description: String? = null
     var imageURL: Int? = null
 }
