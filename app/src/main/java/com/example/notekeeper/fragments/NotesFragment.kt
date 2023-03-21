ebcc4a6f32ef394fb79b18b7c1e0d185b2e126d7
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-21 13:51:31 +0300

    ft: bind the recycler view with the books adapter.

--- app/src/main/java/com/example/notekeeper/fragments/NotesFragment.kt
+++ app/src/main/java/com/example/notekeeper/fragments/NotesFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notekeeper.DataManager
+import com.example.notekeeper.adapters.BooksRecAdapter
 import com.example.notekeeper.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
@@ -25,7 +28,14 @@ class NotesFragment : Fragment() {
         // Inflate the layout for this fragment
         bind = FragmentNotesBinding.inflate(layoutInflater)
 
-
+        val books = DataManager.books
+        val adp = BooksRecAdapter(requireContext())
+        adp.asyncList.submitList(books)
+        bind.recView.apply {
+            adapter = adp
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
         return bind.root
     }
 
