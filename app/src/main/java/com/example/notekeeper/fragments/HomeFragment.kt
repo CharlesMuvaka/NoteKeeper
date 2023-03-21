8012c2a4e30fd133e6c2cb7b1fa7016d299c67d3
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-21 13:46:12 +0300

    ft: iniatilize a book recycler adapter instance.

--- app/src/main/java/com/example/notekeeper/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notekeeper/fragments/HomeFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notekeeper.DataManager
 import com.example.notekeeper.MainActivity
 import com.example.notekeeper.R
+import com.example.notekeeper.adapters.BooksRecAdapter
 import com.example.notekeeper.adapters.NotesRecAdapter
 import com.example.notekeeper.databinding.FragmentHomeBinding
 
@@ -19,6 +22,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         arguments?.let {
         }
     }
@@ -26,17 +31,36 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         bind = FragmentHomeBinding.inflate(layoutInflater)
-        val adapter: NotesRecAdapter? = context?.let { NotesRecAdapter(it, DataManager.notes) }
-        bind.recView.adapter = adapter
-        bind.recView.layoutManager = LinearLayoutManager(context)
+        var adp: NotesRecAdapter? = context?.let { NotesRecAdapter(it, DataManager.books) }
+        bind.recView.adapter = adp
+        bind.recView.layoutManager = GridLayoutManager(requireContext(), 2)
 
-        bind.fab.setOnClickListener(View.OnClickListener {
-            val intent = Intent(context, MainActivity::class.java)
-            context?.startActivity(intent)
-        })
+        bind.transform.startTransformWithDelay(200)
+        bind.transform.bindTargetView(bind.card)
+        bind.transform.startTransform(container!!)
+        bind.transform.finishTransformWithDelay(200)
+
+        val bookAdapter = BooksRecAdapter(requireContext())
+        bookAdapter.asyncList.submitList(DataManager.books)
+        bind.myRecView.apply {
+            adapter = bookAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+
+        }
+
+        bind.fab.setOnClickListener{
+            bind.transform.startTransform()
+            bind.recView.alpha = 0.2F
+            bind.recView.isClickable = false
+
+        }
+
+        bind.card.setOnClickListener{
+            bind.transform.finishTransform()
+        }
         return bind.root
     }
 
