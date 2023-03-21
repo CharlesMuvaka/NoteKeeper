e41ad1f46bb5b29c17386d4b15987b12b8420049
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-21 13:54:23 +0300

    ft: use a tranformation compat instance to start the activity.

--- app/src/main/java/com/example/notekeeper/adapters/NotesRecAdapter.kt
+++ app/src/main/java/com/example/notekeeper/adapters/NotesRecAdapter.kt
@@ -5,40 +5,50 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.notekeeper.MainActivity
-import com.example.notekeeper.NoteInfo
-import com.example.notekeeper.R
+import com.example.notekeeper.*
+import com.skydoves.transformationlayout.TransformationCompat
+import com.skydoves.transformationlayout.TransformationLayout
 
-class NotesRecAdapter(private val context: Context, private val notes:List<NoteInfo>): RecyclerView.Adapter<NotesRecAdapter.ViewHolder>() {
+class NotesRecAdapter(private val context: Context, private val notes:ArrayList<Book>): RecyclerView.Adapter<NotesRecAdapter.ViewHolder>() {
     private val inflator = LayoutInflater.from(context)
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val title: TextView = itemView.findViewById(R.id.noteTitle)
-        val description: TextView = itemView.findViewById(R.id.noteDescription)
+        val image = itemView.findViewById<ImageView>(R.id.image)
+        val title: TextView = itemView.findViewById(R.id.title)
+        val description: TextView = itemView.findViewById(R.id.author)
+        val layout = itemView.findViewById<TransformationLayout>(R.id.transformationLayout)
         var viewPosition = 0
 
-        init {
+        fun setClickListener(note: Book){
             itemView.setOnClickListener{
-                val startActivity = Intent(context, MainActivity::class.java)
+                val startActivity = Intent(context, ToTransformActivity::class.java)
                 startActivity.putExtra("position", viewPosition)
-                context.startActivity(startActivity)
+                startActivity.putExtra("book", note.title)
+                TransformationCompat.startActivity(layout, startActivity)
             }
         }
+
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = inflator.inflate(R.layout.item_notes, parent, false)
+        val view = inflator.inflate(R.layout.book_rec, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val note = notes[position]
-        holder.title.text = note.course.courseTitle
-        holder.description.text = note.noteDescription
+        val authorNames = note.author.split(" ")
+        holder.title.text = note.title
+        holder.description.text = if(note.author.length > 17) "${authorNames[0].subSequence(0,1)}. ${authorNames[1]}" else note.author
+        holder.image.setImageResource(note.imageURL!!)
         holder.viewPosition = position
+
+        holder.setClickListener(note)
 
     }
 
